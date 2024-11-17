17a3d1b5b03a4997d4f1b9474903db6ad13d5cd7
Andrea Fan <andrea.fan@ringus-solution.com>
2024-11-17 23:43:10 +0800

    Amend next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /* eslint-disable import/no-anonymous-default-export */
-
 import { build } from "velite";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+export default {
   // othor next config here...
   webpack: (config) => {
     config.plugins.push(new VeliteWebpackPlugin());
@@ -13,17 +12,19 @@ const nextConfig = {
 
 class VeliteWebpackPlugin {
   static started = false;
-  apply(
-    /** @type {import('webpack').Compiler} */ compiler) {
-    // executed three times in nextjs
+  constructor(/** @type {import('velite').Options} */ options = {}) {
+    this.options = options;
+  }
+  apply(/** @type {import('webpack').Compiler} */ compiler) {
+    // executed three times in nextjs !!!
     // twice for the server (nodejs / edge runtime) and once for the client
     compiler.hooks.beforeCompile.tapPromise("VeliteWebpackPlugin", async () => {
       if (VeliteWebpackPlugin.started) return;
       VeliteWebpackPlugin.started = true;
       const dev = compiler.options.mode === "development";
-      await build({ watch: dev, clean: !dev });
+      this.options.watch = this.options.watch ?? dev;
+      this.options.clean = this.options.clean ?? !dev;
+      await build(this.options); // start velite
     });
   }
 }
-
-export default nextConfig;
