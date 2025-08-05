62e89ab4c4a3a5b35b2fc7fca9d2c7c33899cf2d
Andrea Fan <shan32157@gmail.com>
2025-08-05 18:00:32 +0800

    Refactor HomePageContainer layout and comment out ReactLenis integration

--- src/components/home/HomePageContainer.tsx
+++ src/components/home/HomePageContainer.tsx
@@ -4,28 +4,28 @@ import ProfileContainer from "../../components/profile/ProfileContainer";
 import PostContainer from "@/src/components/profile/PostContainer";
 import WorkExperience from "@/src/components/work-experience/WorkExperience";
 import VolunteerProject from "@/src/components/work-experience/VolunteerProject";
-import { ReactLenis } from "lenis/react";
+// import { ReactLenis } from "lenis/react";
 import { Locales } from "@/src/i18n/request";
 
 export default function HomePageContainer({ locale }: { locale: Locales }) {
   return (
     <>
-      <ReactLenis root>
-        <div className="px-5 flex flex-col gap-32">
-          <section className="mt-32">
-            <ProfileContainer />
-          </section>
-          <section>
-            <PostContainer lang={locale} />
-          </section>
-          <section>
-            <WorkExperience />
-          </section>
-          <section className="mb-[13%]">
-            <VolunteerProject />
-          </section>
-        </div>
-      </ReactLenis>
+      {/* <ReactLenis root> */}
+      <div className="px-5 flex flex-col gap-28">
+        <section className="mt-20 md:mt-28">
+          <ProfileContainer />
+        </section>
+        <section>
+          <PostContainer lang={locale} />
+        </section>
+        <section>
+          <WorkExperience />
+        </section>
+        <section className="mb-[40%] md:mb-[20%]">
+          <VolunteerProject />
+        </section>
+      </div>
+      {/* </ReactLenis> */}
     </>
   );
 }
