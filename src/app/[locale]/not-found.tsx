dd9cef767cfc6ff3de6e18aec9f7e80b00a2d8d7
Andrea Fan <shan32157@gmail.com>
2025-09-07 17:45:11 +0800

    complete article

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -5,14 +5,13 @@ import { useTranslations } from "next-intl";
 export default function NotFound() {
   const t = useTranslations("notFound");
   return (
-    <main className="bg-background w-full max-w-full min-h-screen flex flex-col justify-center items-center">
+    <main className="bg-background w-full mb-[15rem] max-w-full flex flex-col justify-center items-center">
       <h2 className="text-[9rem] md:text-[25rem] relative">
         404
         <div className="text-[9rem] md:text-[25rem] absolute -top-1 left-2 md:-top-1 md:left-5 text-yellow-400 -z-10">
           404
         </div>
       </h2>
-
       <p>Could not find requested resource</p>
       <Link href={ROUTES.HOME} className="font-bold underline">
         {t("returnHome")}
