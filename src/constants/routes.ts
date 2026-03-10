a7b8996bb443daf4560c74ccab2d55df2bd1a891
Andrea Fan <shan32157@gmail.com>
2026-03-10 18:46:32 +0800

    upgrade next and update content

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -3,7 +3,7 @@ const ROUTES = {
   GITHUB: "https://github.com/AndreaFan123",
   LINKEDIN: "https://www.linkedin.com/in/andrea-fan/",
   RESUME:
-    "https://docs.google.com/document/d/1qjNXrEfY_kPXRGgy-hxqM0jXkHBSYJ-_vVuyj2TQJ6Q/edit?usp=sharing",
+    "https://docs.google.com/document/d/1rsOS8gnk_K5JPj8mVyi9cR7J3045t5NbISLHzpgbTAw/edit?usp=sharing",
   SENRYO: "https://order.sen-ryo.com.hk/en",
   MEETUTODAY: "https://meetu.today/en",
   BLOG: "/blog",
